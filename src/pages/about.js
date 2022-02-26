867a2958d0ca3ca264bc607a39bfbbd551c0c178
Augusto Alves <zegutof@gmail.com>
2022-02-26 18:08:35 -0300

    Finalizando o Tutorial parte 2

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="Sobre Nós">
+        <p>Olá, sou o criador deste blog feito com gatsby</p>
+    </Layout>
+  )
+}
+
+export default AboutPage
